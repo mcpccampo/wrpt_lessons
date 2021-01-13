b1d108b4916a3e81dbb40241696df09b6fd8c54c
Camilo Campo <mcp.ccampo@gmail.com>
2021-01-12 20:57:14 -0500

    added review

--- node-1-review/server/bookCtrl.js
+++ node-1-review/server/bookCtrl.js
@@ -1,4 +1,5 @@
 const data = require('../data.json');
+let id = data.length + 1;
 
 module.exports = {
   getAllBooks: (req, res) => {
@@ -18,12 +19,39 @@ module.exports = {
   },
 
   getAllQuery: (req, res) => {
-    console.log('reached getAllQuery Query');
+    console.log('reached getAllQuery EndPoint');
     const { genre } = req.query;
     let booksQuery = [];
     if (genre) {
       booksQuery = data.filter((element) => element.genre === genre);
     }
     res.status(200).send(booksQuery);
+  },
+
+  addBook: (req, res) => {
+    console.log('reach addBook EndPoint');
+    const { author, title, year, cover, genre } = req.body;
+    const newBook = {
+      id,
+      author,
+      title,
+      year,
+      cover,
+      genre,
+    };
+    data.push(newBook);
+    res.status(200).send(data);
+    id++;
+  },
+
+  deleteBook: (req, res) => {
+    const { id } = req.params;
+    let index = data.findIndex((element) => element.id === +id);
+
+    if (index >= 0) {
+      data.splice(index, 1);
+    } else {
+      res.status(404).send('not found...');
+    }
   },
 };
--- node-1-review/server/index.js
+++ node-1-review/server/index.js
@@ -8,6 +8,7 @@ const bookCtrl = require('./bookCtrl');
 app.get('/api/books', bookCtrl.getAllBooks);
 app.get('/api/books/:id', bookCtrl.getBook);
 app.get('/api/booksQuery', bookCtrl.getAllQuery)
+app.post('/api/addBooks', bookCtrl.addBook)
 
 app.listen(3050, () => {
   console.log('App listening on port 3050!');
